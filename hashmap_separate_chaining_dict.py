2af5be96c625e52ced33683dae9d5259cfbcb401
Golichen <2531095701@qq.com>
2025-04-09 16:28:43 +0300

    Update hashmap_separate_chaining_dict.py

--- hashmap_separate_chaining_dict.py
+++ hashmap_separate_chaining_dict.py
@@ -10,12 +10,14 @@ class HashMapSeparateChainingDict:
         self.size = size
         self.capacity = capacity
 
+
     def __eq__(self, other):
         if not isinstance(other, HashMapSeparateChainingDict):
             return False
         # 转换为字典比较内容，忽略顺序
         return dict(to_list(self)) == dict(to_list(other))
         # return dict(to_list(self)) == dict(to_list(other))
+
 
     def __str__(self):
         items = []
@@ -24,8 +26,10 @@ class HashMapSeparateChainingDict:
                 items.append(f"{repr(key)}: {repr(value)}")
         return "{" + ", ".join(items) + "}"
 
+
     def __iter__(self):
         return iterator(self)
+
 
 def cons(key, value, map):
     index = abs(hash(key)) % map.capacity
@@ -36,6 +40,7 @@ def cons(key, value, map):
     new_size = map.size if key_exists else map.size + 1
     return HashMapSeparateChainingDict(new_buckets, new_size, map.capacity)
 
+
 def replace_or_append(bucket, key, value):
     if not bucket:
         return ((key, value),)
@@ -44,6 +49,7 @@ def replace_or_append(bucket, key, value):
         return ((key, value),) + bucket[1:]
     else:
         return (bucket[0],) + replace_or_append(bucket[1:], key, value)
+
 
 def remove(map, key):
     index = abs(hash(key)) % map.capacity
@@ -57,6 +63,7 @@ def remove(map, key):
     new_size = map.size - 1 # if key_existed else map.size
     return HashMapSeparateChainingDict(new_buckets, new_size, map.capacity)
 
+
 def remove_from_bucket(bucket, key):
     if not bucket:
         return ()
@@ -66,10 +73,12 @@ def remove_from_bucket(bucket, key):
     else:
         return (bucket[0],) + remove_from_bucket(bucket[1:], key)
 
+
 def member(key, map):
     index = abs(hash(key)) % map.capacity
     bucket = map.buckets[index]
     return exists_in_bucket(bucket, key)
+
 
 def exists_in_bucket(bucket, key):
     if not bucket:
@@ -77,8 +86,10 @@ def exists_in_bucket(bucket, key):
     current_key, _ = bucket[0]
     return current_key == key or exists_in_bucket(bucket[1:], key)
 
+
 def length(map):
     return map.size
+
 
 def to_list(map):
     def process_buckets(buckets, acc):
@@ -87,6 +98,8 @@ def to_list(map):
         current_bucket = buckets[0]
         new_acc = process_bucket(current_bucket, acc)
         return process_buckets(buckets[1:], new_acc)
+
+
     def process_bucket(bucket, acc):
         if not bucket:
             return acc
@@ -100,6 +113,7 @@ def to_list(map):
     return items
     # return process_buckets(map.buckets, [])
 
+
 def from_list(lst):
     def helper(lst, map):
         if not lst:
@@ -108,14 +122,17 @@ def from_list(lst):
         return helper(lst[1:], cons(key, value, map))
     return helper(lst, empty())
 
+
 def concat(map1, map2):
     list1 = to_list(map1)
     list2 = to_list(map2)
     combined = list1 + list2
     return from_list(combined)
 
+
 def empty():
     return HashMapSeparateChainingDict()
+
 
 def filter(map, predicate):
     def helper(entries, acc):
@@ -128,6 +145,7 @@ def filter(map, predicate):
             return helper(entries[1:], acc)
     return helper(to_list(map), [])
 
+
 def map_func(map, func):
     def helper(entries, acc):
         if not entries:
@@ -135,6 +153,7 @@ def map_func(map, func):
         key, value = entries[0]
         return helper(entries[1:], acc + [(key, func(value))])
     return helper(to_list(map), [])
+
 
 def reduce(map, func, initial):
     def helper(entries, acc):
@@ -144,6 +163,7 @@ def reduce(map, func, initial):
         new_acc = func(acc, (key, value))
         return helper(entries[1:], new_acc)
     return helper(to_list(map), initial)
+
 
 def iterator(map):
     def process_buckets(buckets):
